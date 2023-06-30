2c2b00eb161abcee3b5e528790f21a6175b88a57
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-30 14:06:13 +0300

    added app jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navlayout from './components/Navlayout'
+function App() {
+ 
+
+  return (
+    <>
+     <Navlayout/>
+    </>
+  )
+}
+
+export default App
